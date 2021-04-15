c370fda6afc6c331d152225db978d3dcbfa8f657
codeniu <codeniu@163.com>
2021-04-15 17:03:40 +0800

    feat：useCallback Hook的使用

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import InputWithLabel from '@/components/input-with-label';
 import List from '@/screens/list/list';
 import { useSemiPersistent } from '@/utils/use-semi-persistent';
-import { useEffect, useReducer } from 'react';
+import { useCallback, useEffect, useReducer } from 'react';
 import './App.css';
 
 const App = () => {
-    const initialStories = [
-        {
-            title: 'React',
-            url: 'https://reactjs.org/',
-            author: 'Jordan Walke',
-            num_comments: 3,
-            points: 4,
-            objectID: 0,
-        },
-        {
-            title: 'Redux',
-            url: 'https://redux.js.org/',
-            author: 'Dan Abramov, Andrew Clark',
-            num_comments: 2,
-            points: 5,
-            objectID: 1,
-        },
-    ];
-
+    const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
     const [searchTerm, setSearchTerm] = useSemiPersistent('search', 'react');
-    // const [stories, setStories] = useState([]);
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [isError, setIsError] = useState(false);
-
     const storiesReducer = (state, action) => {
         switch (action.type) {
             case 'STORIES_FETCH_INIT':
@@ -67,18 +45,25 @@ const App = () => {
         isError: false,
     });
 
-    useEffect(() => {
+    const handleFetchStories = useCallback(() => {
+        if (!searchTerm) return;
+
         dispatchStories({ type: 'STORIES_FETCH_INIT' });
-        getAsyncStories()
+
+        fetch(`${API_ENDPOINT}${searchTerm}`)
+            .then(response => response.json())
             .then(result => {
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.data.stories,
+                    payload: result.hits,
                 });
             })
             .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [searchTerm]);
+
+    useEffect(() => {
+        handleFetchStories();
+    }, [handleFetchStories]);
 
     const handleRemoveStory = item => {
         dispatchStories({
@@ -90,14 +75,6 @@ const App = () => {
     useEffect(() => {
         localStorage.setItem('search', searchTerm);
     }, [searchTerm]);
-
-    const getAsyncStories = () => {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve({ data: { stories: initialStories } });
-            }, 2000);
-        });
-    };
 
     const handleSearch = event => {
         setSearchTerm(event.target.value);
