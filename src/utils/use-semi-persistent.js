c3c88dba41f1a23d7a2f63d93dd10eb29f772219
codeniu <codeniu@163.com>
2021-04-15 08:58:34 +0800

    feat：添加自定义半持久化hook

---
+++ src/utils/use-semi-persistent.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+export const useSemiPersistent = (key, initialState) => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+  return [value, setValue];
+};
