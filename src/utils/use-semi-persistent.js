66693f96a06991cf389eec7c92db2695ad1424c6
codeniu <codeniu@163.com>
2021-08-02 11:04:53 +0800

    update

--- src/utils/use-semi-persistent.js
+++ src/utils/use-semi-persistent.js
@@ -1,4 +1,10 @@
 import { useEffect, useState } from 'react';
+
+/**
+ * 半持久化，用于将数据存贮在 localStorge 中
+ * @param {key} String key值
+ * @param {initialState} any 初始值
+ */
 export const useSemiPersistent = (key, initialState) => {
     const [value, setValue] = useState(
         localStorage.getItem(key) || initialState
